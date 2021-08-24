aaebc48120b7623f599ea4e022c59ad6798709e4
leo <leo@leodeMacBook-Pro.local>
2021-08-24 16:27:55 +0800

    update

--- src/main.js
+++ src/main.js
@@ -8,6 +8,17 @@ const fs =require("fs");
 
 /////////ini koa //////////////////////////
 koaApp.use(json());
+koaApp.use(async (ctx, next) => {
+    ctx.set('Access-Control-Allow-Origin', '*');
+    ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    if (ctx.method == 'OPTIONS') {
+        ctx.body = 200;
+    } else {
+        await next();
+    }
+});
+
 koaApp.use(koastatic(ROOTDIR+'/assets/koa_static'));
 koaApp.use(koabodyparser());
 
